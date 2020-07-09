5471864abb99215751f13f32a3c462acca2baa56
user <kelos20@naver.com>
2020-07-09 11:10:39 +0900

    categori

---
+++ app/src/main/java/com/lykong2020/mainselcook/categori.java
@@ -0,0 +1,14 @@
+package com.lykong2020.mainselcook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class categori extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categori);
+    }
+}
